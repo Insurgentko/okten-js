43edab1ba0b2a52f853620304537dd3995022c96
Insurgentko <123051300+Insurgentko@users.noreply.github.com>
2026-08-25 13:04:07 +0200

    classwork 1, homework 1 finished; started classwork 2

---
+++ classwork/Lesson 1/2.variable/scripts/main.js
@@ -0,0 +1,15 @@
+
+//number
+//string
+//boolean
+// переменная типа var доступна во всём файле вне блока инициализации,
+// в отличии от let которая доступна только в своём блоке
+// {
+//     let a = 'hello world';
+//     var b = 'hello okten';
+// }
+// console.log(a)
+// console.log(b)
+
+const CONST=123123123;
+console.log(CONST);
